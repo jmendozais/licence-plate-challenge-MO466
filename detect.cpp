26feeb9414c4fea5145ce671938b7260cc298a82
Julio Mendoza <jmendozais@gmail.com>
2015-06-26 10:01:37 -0300

    Added saving plate rois

--- detect.cpp
+++ detect.cpp
@@ -20,7 +20,7 @@ String window_name = "LPR";
 int main(int args, char* argv[]) 
 {
     if (args < 2) 
-        cout << "Ussage: ./detect img_path" << endl;
+        cout << "Usage: ./detect img_path" << endl;
     string input_path = argv[1];
     Mat input_img = imread(input_path, 1);
     detectAndDisplay(input_img);
@@ -28,21 +28,29 @@ int main(int args, char* argv[])
 
 void detectAndDisplay( Mat frame )
 {
-    std::vector<Rect> faces;
+    std::vector<Rect> plates;
     Mat frame_gray;
 
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
 
     cout << "detecting" << endl;
-    plate_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    plate_cascade.detectMultiScale( frame_gray, plates, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
 
     cout << "end detection" << endl;
-    for ( size_t i = 0; i < faces.size(); i++ )
+    for ( size_t i = 0; i < plates.size(); i++ )
     {
-        cout << "found " << i << endl;
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        stringstream ss;
+        ss << "plates";
+        ss << i;
+        ss << ".jpg";
+        string fout_name;
+        ss >> fout_name;
+        cout << "found " << fout_name << endl;
+
+        Mat plate_roi = frame_gray(plates[i]);
+        imwrite(fout_name, plate_roi);
     }
     imwrite("output.jpg", frame);
 }
+
